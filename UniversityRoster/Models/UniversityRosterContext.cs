8f93ea731b4e5062ef2d03e3dda6540341a712c4
jacobpalaoro <jpalaoro197@gmail.com>
2022-08-01 14:05:44 -0700

    add universityrostercontext file with boiler code.
    Co-authored-by: Jonathan Lu <jdlu.coding@gmail.com>

---
+++ UniversityRoster/Models/UniversityRosterContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace UniversityRoster.Models
+{
+  public class UniversityRosterContext : DbContext
+  {
+    public DbSet<Course> Courses { get; set; }
+    public DbSet<Student> Students { get; set; }
+    public DbSet<CourseStudent> CourseStudent { get; set; }
+
+    public UniversityRosterContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
+  }
+}
