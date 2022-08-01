85c884d54a2bd9c2dfe436ff3fe1200c57b6bffb
jacobpalaoro <jpalaoro197@gmail.com>
2022-08-01 14:33:33 -0700

    add student controller with minimal code.
    Co-authored-by: Jonathan Lu <jdlu.coding@gmail.com>

---
+++ UniversityRoster/Controllers/StudentController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using UniversityRoster.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UniversityRoster.Controllers
+{
+  public class StudentsController : Controller
+  {
+    private readonly UniversityRosterContext _db;
+
+   
+  }
+}
